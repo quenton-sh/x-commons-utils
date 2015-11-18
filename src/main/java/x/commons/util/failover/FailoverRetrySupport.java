0f59ff9848181595c61497883b507bec7b3113a6
Quenton Shang <quenton2010@gmail.com>
2015-11-18 12:17:58 +0800

    添加FailoverRetrySupport组件

---
+++ src/main/java/x/commons/util/failover/FailoverRetrySupport.java
@@ -0,0 +1,59 @@
+package x.commons.util.failover;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import x.commons.util.failover.FailoverCallable;
+import x.commons.util.failover.FailoverSupport;
+import x.commons.util.failover.RetrySupport;
+
+public class FailoverRetrySupport<RESOURCE_TYPE> {
+	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	protected final FailoverSupport<RESOURCE_TYPE> failoverSupport;
+	protected final RetrySupport retrySupport;
+
+	public FailoverRetrySupport(List<RESOURCE_TYPE> resourceList, int failRetryCount, int failRetryIntervalMillis) {
+		this.failoverSupport = new FailoverSupport<RESOURCE_TYPE>(resourceList) {
+			@Override
+			protected void logException(Exception e, int index, boolean hasNext) {
+				FailoverRetrySupport.this.logFailoverException(e, index, hasNext);
+			}
+		};
+		this.retrySupport = new RetrySupport(failRetryCount, failRetryIntervalMillis) {
+			@Override
+			protected void logException(Exception e, int leftRetryTime) {
+				FailoverRetrySupport.this.logRetryException(e, leftRetryTime);
+			}
+		};
+	}
+	
+	public <RETURN_TYPE> RETURN_TYPE callWithFailoverRetry(
+			final FailoverCallable<RETURN_TYPE, RESOURCE_TYPE> callable) throws Exception {
+		return this.failoverSupport.callWithFailover(new FailoverCallable<RETURN_TYPE, RESOURCE_TYPE>() {
+			@Override
+			public RETURN_TYPE call(final RESOURCE_TYPE resource, final int resourceIndex) throws Exception {
+				return retrySupport.callWithRetry(new Callable<RETURN_TYPE>() {
+					@Override
+					public RETURN_TYPE call() throws Exception {
+						return callable.call(resource, resourceIndex);
+					}
+				});
+			}
+		});
+	}
+
+	protected void logFailoverException(Exception e, int index, boolean hasNext) {
+		String msg = this.failoverSupport.buildExceptionMsg(e, index, hasNext);
+		logger.error(msg, e);
+	}
+	
+	protected void logRetryException(Exception e, int leftRetryTime) {
+		String msg = this.retrySupport.buildExceptionMsg(e, leftRetryTime);
+		logger.error(msg, e);
+	}
+}
---
+++ src/test/java/x/commons/util/failover/FailoverRetrySupportTest.java
@@ -0,0 +1,87 @@
+package x.commons.util.failover;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Test;
+
+public class FailoverRetrySupportTest {
+
+	@Test
+	public void test() throws Exception {
+		// 0、1资源无效，2有效
+		// 第1、2次执行无效，第三次执行有效
+		List<String> resourceList = Arrays.asList("ip0", "ip1", "ip2");
+		final AtomicInteger tryCount = new AtomicInteger(0);
+		final ThreadLocal<String> resourceRef = new ThreadLocal<String>();
+		FailoverCallable<Integer, String> callable = new FailoverCallable<Integer, String>() {
+			@Override
+			public Integer call(String resource, int index)
+					throws Exception {
+				if (resourceRef.get() != resource) {
+					// 说过刚刚failover切换到新资源上
+					tryCount.set(0);
+					resourceRef.set(resource);
+					System.out.println("now try resource=" + resource);
+				}
+				if (tryCount.incrementAndGet() <= 3) {
+					System.out.println("try time: " + tryCount.get() + " going to throw an exception.");
+					throw new Exception("try=" + tryCount.get());
+				}
+				System.out.println("try time: " + tryCount.get() + " succeeded.");
+				assertEquals("ip" + index, resource);
+				if (index == 0 || index == 1) {
+					System.out.println("index: " + index + " going to throw an exception.");
+					throw new Exception("index=" + index);
+				}
+				return index;
+			}
+		};
+		
+		// 重试两次，失败
+		tryCount.set(0);
+		FailoverRetrySupport<String> sug = new FailoverRetrySupport<String>(resourceList, 2, 0) {
+			@Override
+			protected void logFailoverException(Exception e, int index, boolean hasNext) {
+				String msg = this.failoverSupport.buildExceptionMsg(e, index, hasNext);
+				System.out.println(msg);
+			}
+			
+			@Override
+			protected void logRetryException(Exception e, int leftRetryTime) {
+				String msg = this.retrySupport.buildExceptionMsg(e, leftRetryTime);
+				System.out.println(msg);
+			}
+		};
+		try {
+			sug.callWithFailoverRetry(callable);
+			fail();
+		} catch (Exception e) {
+			System.out.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+
+		System.out.println("--------------------");
+		
+		// 重试4次，成功
+		sug = new FailoverRetrySupport<String>(resourceList, 4, 0) {
+			@Override
+			protected void logFailoverException(Exception e, int index, boolean hasNext) {
+				String msg = this.failoverSupport.buildExceptionMsg(e, index, hasNext);
+				System.out.println(msg);
+			}
+			
+			@Override
+			protected void logRetryException(Exception e, int leftRetryTime) {
+				String msg = this.retrySupport.buildExceptionMsg(e, leftRetryTime);
+				System.out.println(msg);
+			}
+		};
+		int ret = sug.callWithFailoverRetry(callable);
+		assertTrue(ret == 2);
+	}
+}
